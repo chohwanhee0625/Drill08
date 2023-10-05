a496cab1b303d299b6f323d7c7391f0de71c3f64
chohwanhee(lggram) <great1625@gmail.com>
2023-10-05 12:22:58 +0900

    게임 기본 로직 준비 및 잔디 클래스 선언

--- boy_grass_object.py
+++ boy_grass_object.py
@@ -1,6 +1,14 @@
 from pico2d import *
 
+
 # Game object class here
+class Grass:
+    def __init__(self):
+        self.image = load_image('grass.png')
+
+    def draw(self):
+        self.image.draw(400, 30)
+
 
 def handle_events():
     global running
@@ -11,11 +19,33 @@ def handle_events():
         elif event.type == SDL_KEYDOWN and event.key == SDLK_ESCAPE:
             running = False
 
+
+def reset_world():
+    global running
+    running = True
+
+
+def render_world():
+    clear_canvas()
+    update_canvas()
+
+
+def update_world():
+    pass
+
+
 open_canvas()
 
 # initialization code
+reset_world()
 
 # game main loop code
+while running:
+    handle_events()
+    update_world()          # 객체들의 상호작용 결과 업데이트
+    render_world()
+
+    delay(0.01)
 
 # finalization code
 
